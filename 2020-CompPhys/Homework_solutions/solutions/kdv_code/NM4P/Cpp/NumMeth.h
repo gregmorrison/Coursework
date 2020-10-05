cdf33ea214c669385031ea3869084ca6c1dcab76
Greg Morrison <gcmorrison@uh.edu>
2020-10-05 00:01:42 -0500

    updates for ch4-5, bug fixes

--- 2020-CompPhys/Homework_solutions/solutions/kdv_code/NM4P/Cpp/NumMeth.h
+++
@@ -1,8 +0,0 @@
-// General header file for C++ programs 
-// in "Numerical Methods for Physics" 
-
-#include <iostream.h>
-#include <fstream.h>
-#include <assert.h>  
-#include <math.h>
-#include "Matrix.h"
